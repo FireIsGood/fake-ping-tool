13b22c10591f871abe9e1971af889f969f156ecb
FireIsGood <fireisadequate@gmail.com>
2025-03-31 23:17:31 -0700

    Initial commit

---
+++ script.mjs
@@ -0,0 +1,21 @@
+$(function () {
+  $("#file-upload").on("change", (event) => {
+    const file = event.target.files[0];
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onloadend = function (e) {
+      const dataUrl = e.target.result;
+      console.log(dataUrl);
+      $("#server-icon").attr("src", dataUrl);
+    };
+  });
+
+  $("#save-file").click(() => {
+    modernScreenshot.domToPng($("#server-icon-full")[0]).then((dataUrl) => {
+      const link = document.createElement("a");
+      link.download = $("#file-name").val() + ".png";
+      link.href = dataUrl;
+      link.click();
+    });
+  });
+});
